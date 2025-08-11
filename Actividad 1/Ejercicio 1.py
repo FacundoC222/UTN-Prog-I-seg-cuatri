5407708c06826a53c5f72d103b8243d8f0a286e8
FacundoC222 <facuuundo2017@gmail.com>
2025-08-11 06:43:28 -0300

    Add files via upload

---
+++ Actividad 1/Ejercicio 1.py
@@ -0,0 +1,62 @@
+#Ejercicio 1
+#print("Hola mundo")
+
+#Ejercicio 2
+#nombre=input("Ingrese su nombre: ")
+#print("hola",nombre)
+
+#Ejercicio 3
+#nombre=input("ingrese su nombre: ")
+#apellido=input("ingrese su apellido: ")
+#edad=input("ingrese su edad: ")
+#lugar_recidencia=input("donde reside? ")
+#print(f"Soy {nombre} {apellido} tengo {edad} y resido en {lugar_recidencia}")
+
+#Ejercicio 4
+#radio=float(input("Ingrese el radio del circulo: "))
+#area=3.14*radio**2
+#perimetro=2*3.14*radio
+#print(f"El area del circulo es {area} y el perimetro es {perimetro}")
+
+#Ejercicio 5
+#segundos=int(input("Ingrese los segundos: "))
+#hora=segundos/3600
+#print(f"{segundos} equivalen a {hora} horas")
+
+#Ejercicio 6
+#numero=int(input("Ingrese un numero: "))
+#contador=0
+#while contador < 11:
+#    tabla = numero * contador
+#    print (f"{numero}*{contador} = {tabla}")
+#    contador=contador+1
+
+#Ejercicio 7
+#numero1=int(input("Ingrese un numero mayor a 0: "))
+#numero2=int(input("Ingrese otro numero mayor a 0: "))
+#if numero1>0 and numero2>0:
+#    suma=numero1+numero2
+#    resta=numero1-numero2
+#    multiplicacion=numero1*numero2
+#    divicion=numero1/numero2
+#    print(f"El total de la suma es {suma} el de la resta {resta} el de la multiplicacion {multiplicacion} y el de la divicion {divicion}")
+#else:
+#    print("ingreso un numero igual o menor a 0")
+
+#Ejercicio 8
+#peso=float(input("Ingrese su peso en kg: "))
+#altura=float(input("Ingrese su altura en metros: "))
+#imc=peso/altura**2
+#print(f"Su IMC es de {imc}")
+
+#Ejercicio 9
+#tempC=float(input("Ingrese la temperatura en Celcius: "))
+#tempF=(9/5)*tempC+32
+#print(f"La temperatura en fahrenheit es {tempF}")
+
+#Ejercicio 10
+#num1=int(input("Ingrese el primer numero: "))
+#num2=int(input("Ingrese el segundo numero: "))
+#num3=int(input("Ingrese el tercer numero: "))
+#promedio=(num1+num2+num3)/3
+#print(f"El promedio de los numeros es {promedio}")
